8bf3c2da8f54187f89f8df8728c1af03c25cec87
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2019-11-11 16:23:24 +0100

    UI-Styles initialisieren, AudioUtils initialisieren und disposen

--- core/src/com/mygdx/zombiejump/Zombiejump.java
+++ core/src/com/mygdx/zombiejump/Zombiejump.java
@@ -6,25 +6,40 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.I18NBundle;
 import com.mygdx.zombiejump.base.BaseGame;
+import com.mygdx.zombiejump.base.BaseUI;
 import com.mygdx.zombiejump.screen.MenuScreen;
+import com.mygdx.zombiejump.utils.AudioUtils;
 
 public class Zombiejump extends BaseGame {
 
     public static int health;
-	public static int coinsCount;
+    public static int coinsCount;
     public static int zombieCount;
     public static I18NBundle myBundle;
 
     @Override
     public void create() {
         super.create();
-        
+
         FileHandle baseFileHandle = Gdx.files.internal("i18n/lang");
-        //Locale locale = new Locale("ru");
-        //myBundle = I18NBundle.createBundle(baseFileHandle, locale);
+        // Locale locale = new Locale("ru");
+        // myBundle = I18NBundle.createBundle(baseFileHandle, locale);
         myBundle = I18NBundle.createBundle(baseFileHandle, Locale.getDefault());
 
-        setActiveScreen( new MenuScreen() );
+        BaseUI.loadAssets();
+        
+        AudioUtils.getInstance().init();
+
+        setActiveScreen(new MenuScreen());
     }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+
+        AudioUtils.dispose();
+    }
+
+    
 
 }
