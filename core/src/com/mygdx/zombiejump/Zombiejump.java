a66cd1b7d2d870edec0ebe3b8b1e4fbd5c3d1c15
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2019-11-11 09:50:04 +0100

    language bundles

--- core/src/com/mygdx/zombiejump/Zombiejump.java
+++ core/src/com/mygdx/zombiejump/Zombiejump.java
@@ -1,7 +1,10 @@
 package com.mygdx.zombiejump;
 
+import java.util.Locale;
+
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.utils.I18NBundle;
 import com.mygdx.zombiejump.base.BaseGame;
 import com.mygdx.zombiejump.screen.MenuScreen;
 
@@ -10,11 +13,16 @@ public class Zombiejump extends BaseGame {
     public static int health;
 	public static int coinsCount;
     public static int zombieCount;
-    
+    public static I18NBundle myBundle;
+
     @Override
     public void create() {
         super.create();
         
+        FileHandle baseFileHandle = Gdx.files.internal("i18n/lang");
+        //Locale locale = new Locale("de", "DE");
+        myBundle = I18NBundle.createBundle(baseFileHandle, Locale.getDefault());
+
         setActiveScreen( new MenuScreen() );
     }
 
