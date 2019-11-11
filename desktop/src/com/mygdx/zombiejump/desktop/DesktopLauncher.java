3b30618253fb6ef42a3353e98a111f9fa8e4abbd
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2019-11-11 16:19:52 +0100

    breite und länge aus constanten

--- desktop/src/com/mygdx/zombiejump/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/zombiejump/desktop/DesktopLauncher.java
@@ -3,13 +3,14 @@ package com.mygdx.zombiejump.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.zombiejump.Zombiejump;
+import com.mygdx.zombiejump.utils.Constants;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "ZJ";
-		config.width = 800;
-		config.height = 600;
+		config.title = Constants.GAME_TITLE;
+		config.width = Constants.GAME_WINDOW_WIDTH;
+		config.height = Constants.GAME_WINDOW_HEIGHT;
 		new LwjglApplication(new Zombiejump(), config);
 	}
 }
