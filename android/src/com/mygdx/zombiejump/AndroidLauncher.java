b387e4053ab52ef287fc6a4b49d5682f1ce8ef66
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2019-11-14 15:49:03 +0100

    startclasse = mygame

--- android/src/com/mygdx/zombiejump/AndroidLauncher.java
+++ android/src/com/mygdx/zombiejump/AndroidLauncher.java
@@ -13,6 +13,6 @@ public class AndroidLauncher extends AndroidApplication {
 		config.useAccelerometer = false;
 		config.useCompass = false;
 		config.useGyroscope = false;
-		initialize(new ZombieJump(), config);
+		initialize(new MyGame(), config);
 	}
 }
